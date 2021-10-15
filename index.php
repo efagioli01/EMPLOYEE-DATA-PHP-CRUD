260f938afc0b2776d622e3a7734f9c78681b17ec
Erica Fagioli <efagioli01@gmail.com>
2021-10-15 09:07:16 -0400

    new php app

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<p>this is a paragraph
+<?php
+
+echo "Hello World!";
+echo "this is also a paragraph";
+echo "\n";
+echo "i am working i think";
+
+
+?>
+<p>this is a para</p>
+<h1>hi</h1>
+<h2>this is txt</h2>
+</body>
+</html>
